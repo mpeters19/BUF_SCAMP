f8f539dfe9651e5eb5b2800ff151631d9c2fb9d6
McKenzie Peters <mp46@illinois.edu>
2023-09-08 09:22:47 -0500

    Adding plotting files

--- Processing/ParsivelPSDBUF.py
+++ Processing/ParsivelPSDBUF.py
@@ -29,8 +29,8 @@ def calc_dsd(date):
 
     Use to get the data to make plots
     '''
-    ds_10s = xr.open_mfdataset('/data/accp/a/snesbitt/scamp/parsivel/nc_daily/'+date+'_*_SCAMP_Parsivel.nc')
-    ds = ds_10s.resample(time='1MIN').sum()
+    ds_10s = xr.open_mfdataset('/data/accp/a/snesbitt/scamp/parsivel/nc_daily/'+date+'_*_SCAMP_Parsivel.nc') #opens the data files
+    ds = ds_10s.resample(time='1MIN').sum() #data is originally recorded every 10s, this resamples it to 1 min by summing all data within a minute
 
     dsd = ParsivelPSDBUF(ds)
 
@@ -70,19 +70,15 @@ class ParsivelPSDBUF(object):
         #some data stored as tuples if applicable
         #first element = average or total across all 32 DSD bins
         #second element = value for each bin individually (plus time-dimension)
-        self.ds = ds
+        self.ds = ds #initializing ds as the original disdrometer data file
         self.time = self.ds.time 
         self.timedim = len(ds.time) #time dimension
-        #ndrops is the number of drops (non-normalzed) in each volume of air
         #DSD is the number of drops per volume of air for a given drop size interval (i.e. for the 32 bins)
         self.dsd = np.zeros((self.timedim,32)) #dsd 
-        self.vvd = np.zeros(self.timedim)
-        self.mass_spectrum_warm = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
-        self.mass_spectrum_cold = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
-        self.mass_spectrum_conv = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
+        self.mass_spectrum_warm = (np.zeros(self.timedim),np.zeros((self.timedim,32))) #mass spectrum using Heymsfield warm coefficients 
+        self.mass_spectrum_cold = (np.zeros(self.timedim),np.zeros((self.timedim,32)))  #mass spectrum using Heymsfield cold coefficients 
+        self.mass_spectrum_conv = (np.zeros(self.timedim),np.zeros((self.timedim,32))) #mass spectrum using Heymsfield convective coefficients 
         self.IWC = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
-        self.LWC = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
-        self.lwc = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
         self.Williams_LWC = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
         self.liquid_mass = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
         self.liquid_mass_spectrum = (np.zeros(self.timedim),np.zeros((self.timedim,32)))
@@ -97,12 +93,6 @@ class ParsivelPSDBUF(object):
         self.moments = np.zeros((self.timedim,8)) #drop moments 
         self.D0 = np.zeros(self.timedim)
         #self.Dmax = np.zeros(self.timedim)
-        self.Dm = np.zeros(self.timedim)
-        self.Nt = np.zeros(self.timedim)
-        self.Nw = np.zeros(self.timedim)
-        self.N0 = np.zeros(self.timedim)
-        self.mu = np.zeros(1)
-        self.Lambda = np.zeros(1)
 
     def get_precip_params(self):
         
@@ -121,10 +111,7 @@ class ParsivelPSDBUF(object):
             time_mult = 60  #units: seconds
             #time_div = 60 #units: s/min
             matrix = np.array(self.ds.spectrum[td,:,:])
-            vvd_num = 0
-            vvd_denom = 0
             
-            #ndrops = np.sum(matrix)
             #Process each drop bin (diameter, velocity):
             for dind,dbin in enumerate(self.drop_diameter):
                 for vind,vbin in enumerate(self.v_parsivel):
@@ -147,15 +134,11 @@ class ParsivelPSDBUF(object):
                     moment6 = np.power(self.drop_diameter[dind], 6)
                     self.moments[td,6] += np.multiply(np.multiply(moment6, self.dsd[td,dind]), self.drop_spread[dind])
                     
-                    vol = np.pi*dbin**3/6 #volume of 1 drop in this size bin
-                    self.lwc[1][td,dind] += drops*vol*1.e3/denominator #units: g/m^3 per mm bin (rho=1000 g/m^3)
                     #reflectivity factor (6th power of diameter, normalized for area, time)
                     self.z[1][td,dind] += drops * 1.e6 * dbin**6/denominator
                     # equivalent reflectivity, Smith 1984
                     self.ze[1][td,dind] += (0.176/0.93)*self.z[1][td,dind]
-                    #reflectivity weighted vvd
-                    vvd_num += np.sum(moment6 * (1.e6 * drops)/denom2 * self.drop_spread[dind] * vbin)
-                    vvd_denom += np.sum(moment6 * (1.e6 * drops)/denom2 * self.drop_spread[dind])
+                    
                     # using PyDSD
                     #self.rainrate[1][td,dind] += 0.6 * np.pi * 1e-03 * np.dot(np.multiply(self.proc_p2.rainrate[td],np.multiply(self.ndrops[0][td],self.drop_spread[dind])),np.array(self.drop_diameter[dind]) ** 3)
 
@@ -175,12 +158,6 @@ class ParsivelPSDBUF(object):
             dm_denominator = np.sum(self.heymsfield_warm_mass*self.dsd[td]*self.drop_spread)
             self.Dm[td] = np.divide(dm_numerator,dm_denominator)
 
-            #VVD
-            self.vvd[td] = np.divide(vvd_num, vvd_denom)
-            #vvd_numerator = np.sum(self.heymsfield_warm_mass*self.drop_diameter_melted*self.dsd[td]*self.drop_spread)
-            #vvd_denominator = np.sum(self.heymsfield_warm_mass*self.dsd[td]*self.drop_spread)
-            #self.Dm[td] = np.divide(dm_numerator,dm_denominator)
-
             #ice mass, Matrosov 2007
             self.mass_spectrum_matrosov[1][td,0:13] = np.multiply(self.dsd[td,0:13],self.matrosov_001_02_mass)
             self.mass_spectrum_matrosov[1][td,13:30] = np.multiply(self.dsd[td,13:30],self.matrosov_02_2_mass)
@@ -188,33 +165,11 @@ class ParsivelPSDBUF(object):
             self.mass_spectrum_matrosov[0][td] = np.sum(self.mass_spectrum_matrosov[1][td,1:32])
 
             # IWC, Heymsfield 2005
-            #mass_multiplication = np.multiply(np.divide(self.dsd[td],10^5),self.heymsfield_warm_mass)
             variable_multiplcation = np.multiply(self.dsd[td],self.heymsfield_warm_mass)
             self.IWC[1][td] = np.multiply(self.drop_spread,variable_multiplcation)
             self.IWC[1][td] = np.multiply(self.IWC[1][td],10^6)
-            #self.IWC[1][td] = np.multiply(self.mass_warm[1][td],self.drop_spread)
             self.IWC[0][td] = np.sum(self.IWC[1][td,:])
             
-            '''
-            # LWC, Lamb and Verlinde, lecture 1 slide 41 of 510
-            moments_for_LWC = np.power(np.divide(self.drop_diameter,10),3)
-            coefficient = np.divide(np.pi*1,6) #density here is 1 g/cm^3 
-            following_sigma = np.multiply(moments_for_LWC,self.dsd[td])
-            self.LWC[1][td] = np.multiply(coefficient,following_sigma)
-            self.LWC[1][td] = np.multiply(self.LWC[1][td],self.drop_spread)
-            self.LWC[0][td] = np.sum(self.LWC[1][td,:])
-            
-            # Solving for mass using density = M/V
-            moment_for_mass = np.power(np.divide(self.drop_diameter,10),3) #convert diameters from mm to cm, then put to the power of 3
-            self.liquid_mass_simple[1][td,:] = (1/6)*np.pi*moment_for_mass*1 #multiply diameters^3 by pi/6 times 1 g/cm^3
-            self.liquid_mass_simple[0][td] = np.sum(self.liquid_mass_simple[1][td,:])
-            self.liquid_mass_spectrum_simple[1][td,:] = np.multiply(self.dsd[td],self.liquid_mass_simple[1][td]) # multiply the previous product by the respective number concentration of that diameter and time index
-            self.liquid_mass_spectrum_simple[0][td] = np.sum(self.liquid_mass_spectrum_simple[1][td,:]) # sum mass spectrums from each bin to get a total number for the time index
-            
-            
-            self.LWC[1][td] = ((1/6) * np.pi * 1e-03 * self.dsd[td] * self.drop_spread * np.array(self.drop_diameter) ** 3)
-            self.LWC[0][td] = np.sum(self.LWC[1][td,:])
-            '''
             # liquid mass, Williams et al. 2014
             moments_for_liquid_mass = np.power(self.drop_diameter,3) #diameters in mm to the power of 3
             coefficient = np.divide(np.pi*1,6*1000) #calculating the coefficient, density here is 1 g/cm^3 
@@ -233,7 +188,6 @@ class ParsivelPSDBUF(object):
             #self.z[1][td] = np.multiply(self.dsd[td],np.power(self.drop_diameter,6))
             #self.z[1][td] = np.multiply(self.z[1][td],self.drop_spread)
             #self.z[1][td] = np.power(self.drop_diameter,6)
-            self.lwc[0][td] = np.sum(self.lwc[1][td,:])
             self.z[0][td] = np.sum(self.z[1][td,:])
             if self.z[0][td] > 0:
                 self.dbz[td] = 10 * np.log10(self.z[0][td]) #z to dbz
